aabb2304239f7b3d747cfb1dccf87ae0ae0c4e34
Vishnu-priyan <vishnu13126@cse.ssn.edu.in>
2016-12-28 17:06:43 +0530

    Update emailpy.py

--- emailpy.py
+++ emailpy.py
@@ -1,6 +1,8 @@
 import re
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+#Change the url to the site you need
+#Multiple urls are simple too give them in a list and iterate the list lulz :)
 html = urlopen("http://unseenrajasthan.blogspot.in/")
 soup = BeautifulSoup(html,"html.parser")
 text = soup.find('body').get_text().strip()
